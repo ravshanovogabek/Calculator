4cabf8aa6ec29dfe255854c94d28cc113478409b
ravshanovogabek <140580695+ravshanovogabek@users.noreply.github.com>
2024-06-22 07:38:57 +0500

    my calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState('');
+
+  const handleButtonClick = (e) => {
+    const val = e.target.value;
+    if (val === '=') {
+      try {
+        setValue(eval(value).toString());
+      } catch {
+        setValue('Error');
+      }
+    } else {
+      setValue(value + val);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="calculator">
+        <div className="display">
+          <input type="text" value={value} readOnly />
+        </div>
+        <div className="buttons">
+          <input type="button" value="AC" className="span-two" onClick={() => setValue('')} />
+          <input type="button" value="DE" onClick={() => setValue(value.slice(0, -1))} />
+          <input type="button" value="/" className="operator" onClick={handleButtonClick} />
+          <input type="button" value="7" onClick={handleButtonClick} />
+          <input type="button" value="8" onClick={handleButtonClick} />
+          <input type="button" value="9" onClick={handleButtonClick} />
+          <input type="button" value="*" className="operator" onClick={handleButtonClick} />
+          <input type="button" value="4" onClick={handleButtonClick} />
+          <input type="button" value="5" onClick={handleButtonClick} />
+          <input type="button" value="6" onClick={handleButtonClick} />
+          <input type="button" value="-" className="operator" onClick={handleButtonClick} />
+          <input type="button" value="1" onClick={handleButtonClick} />
+          <input type="button" value="2" onClick={handleButtonClick} />
+          <input type="button" value="3" onClick={handleButtonClick} />
+          <input type="button" value="+" className="operator" onClick={handleButtonClick} />
+          <input type="button" value="00" onClick={handleButtonClick} />
+          <input type="button" value="0" onClick={handleButtonClick} />
+          <input type="button" value="." onClick={handleButtonClick} />
+          <input type="button" value="=" className="span-two operator" onClick={handleButtonClick} />
+        </div>
+      </div>
     </div>
   );
 }
